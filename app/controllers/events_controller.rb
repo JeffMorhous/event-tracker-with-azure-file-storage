90b1fdd26fcc94870b41d42bc1a632c96fa40f75
Jeff Morhous <morhous.jeff@gmail.com>
2024-01-11 23:03:23 -0500

    Add honeybadger error monitoring and an intentional bug

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    @events = Event.all
+    @events = Birthday.all
   end
 
   # GET /events/1 or /events/1.json
